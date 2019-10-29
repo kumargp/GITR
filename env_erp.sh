0208fa90fbb1b2318ffaf77e88d5310be4a587fa
Gopan Perumpilly <gopan.p@gmail.com>
2019-10-29 15:02:35 -0400

    erp run scripts

---
+++ env_erp.sh
@@ -0,0 +1,8 @@
+module use /gpfs/u/software/erp-spack-install/lmod/linux-centos7-x86_64/Core/
+module load gcc
+module load cmake
+module load openmpi
+module load thrust/1.8.2-lespvug
+module load libconfig
+module load netcdf-cxx4
+module load boost
